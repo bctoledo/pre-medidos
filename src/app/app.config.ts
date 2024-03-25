89fd84221987a079779302941880c1f1c14f5d04
Bruno Cesar Gonçalves de Toledo <bruno@inmetroms.gov.br>
2024-03-25 12:32:07 -0400

    add material angular 17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerLocaleData(localePt, 'pt-BR');
 
@@ -12,6 +13,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
-  ],
+    { provide: LOCALE_ID, useValue: 'pt-BR' }, provideAnimationsAsync(), provideAnimationsAsync()
+  ]
 };
