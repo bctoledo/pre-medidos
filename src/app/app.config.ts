c5ee8bde4dc722fc90b4aa17bdf3bdf68a841e29
Bruno Cesar Gonçalves de Toledo <bruno@inmetroms.gov.br>
2024-03-25 10:40:11 -0400

    Locale pt-br

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
 };
--- src/app/components/teste-portas/teste-portas.component.ts
+++ src/app/components/teste-portas/teste-portas.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { NgxSerial } from 'ngx-serial';
+import { NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-teste-portas',
   templateUrl: './teste-portas.component.html',
-  styleUrls: ['./teste-portas.component.css']
+  styleUrls: ['./teste-portas.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class TestePortasComponent implements OnInit {
-  portasDisponiveis: string[] = [];
-  port: any;
 
+  port: any;
   serial: NgxSerial;
+
   pesoAtual: string;
+  leituras: number[] = [];
 
   constructor() {
     let options = { baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none', stopBits: 1 };
@@ -21,13 +26,14 @@ export class TestePortasComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //this.listarPortasDisponiveis();
+    // this.realizarLeituras();
   }
 
-  dataHandler(data: string) {
+  dataHandler = (data: string) => {
     console.log("From balanca -> " + data);
     this.pesoAtual = data.toString();
   }
+
 
   connect() {
     if (!this.port) {
@@ -38,20 +44,41 @@ export class TestePortasComponent implements OnInit {
   }
 
   toggleL1() {
-    debugger
-    if (this.port)
+    if (this.port) {
       //this.pesoAtual = this.serial.connect; //sendData("L1\n"); //L1\n
 
-      debugger;
+      const regex = /(\d+(\.\d+)?)/; // Expressão regular para encontrar números decimais
+      const matches = this.pesoAtual.match(regex);
+
+      if (matches && matches.length > 1) {
+        const number = parseFloat(matches[1]);
+        console.log(number); // Output: 313.1
+        this.leituras.push(number);
+      } else {
+        console.log("Nenhum número encontrado na string.");
+      }
+    }
   }
 
   close() {
     if (this.port)
       this.serial.close((port: any) => {
         this.port = port;
-
+        this.pesoAtual = "";
       });
   }
+
+  // realizarLeituras() {
+
+  //   if (this.port) {
+  //     this.leituras = [];
+  //     for (let i = 0; i < 5; i++) {
+  //       let leitura = "Leitura " + (i + 1);
+  //       this.leituras.push(leitura);
+  //       debugger
+  //     }
+  //   }
+  // }
 
   //   listarPortasDisponiveis() {
   //     this.serial.list()
